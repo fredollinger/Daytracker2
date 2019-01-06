33af4e298ebf8d908a1044ccfcb1bd85a27a48a9
Frederick Ollinger <follinge@gmail.com>
2019-01-05 20:17:56 -0800

    Change default displayed message.

--- app/src/main/java/com/fredollinger/day_tracker2/MainActivity.kt
+++ app/src/main/java/com/fredollinger/day_tracker2/MainActivity.kt
@@ -2,14 +2,17 @@ package com.fredollinger.day_tracker2
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val d = new DTDate()
-        var t = d.getCurrentTimeSeconds()
-        var tvMain = findViewById(R.id.textViewMain) as textViewMain
         setContentView(R.layout.activity_main)
+        val d = DTDate()
+        var t = d.getCurrentTimeSeconds()
+        textView.setText("hi")
+        //tvMain.setText("Days Elapsed")
+        // var button = (TextView) findViewById(R.id.textView);
     }
 }
