47418624e64e9deb76ae30e60b5343815c37c64e
Frederick Ollinger <follinge@gmail.com>
2019-01-06 16:56:24 -0800

    Add STUB menu item.

--- app/src/main/java/com/fredollinger/day_tracker2/MainActivity.kt
+++ app/src/main/java/com/fredollinger/day_tracker2/MainActivity.kt
@@ -3,6 +3,8 @@ package com.fredollinger.day_tracker2
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import android.view.Menu
+import android.view.MenuInflater
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,7 +13,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val d = DTDate()
         textView.setText("Days Elapsed: " + d.daysElapsedString())
-        //tvMain.setText("Days Elapsed")
-        // var button = (TextView) findViewById(R.id.textView);
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.options_menu, menu)
+        return true
+    }
+
+
 }
