1f812201e0252c440ffa65370b2f3d7540bad05d
Frederick Ollinger <follinge@gmail.com>
2019-01-10 09:58:39 -0800

    Now read init t from file if not save current time

--- app/src/main/java/com/fredollinger/day_tracker2/DTDate.java
+++ app/src/main/java/com/fredollinger/day_tracker2/DTDate.java
@@ -1,7 +1,9 @@
 package com.fredollinger.day_tracker2;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.Date;
 import java.util.Calendar;
@@ -17,9 +19,16 @@ public class DTDate {
         loadInitialTime();
     }
 
-    // TODO: Actually load a time STUB for testing
     public void loadInitialTime() {
-        initTime = 1546022333;
+	File f = new File(saveFileName);
+	if(f.exists() && !f.isDirectory()) { 
+	    System.out.println("dir exists");
+            initTime = readInitTimeFromFile();
+        }
+	else {
+            initTime = getCurrentTimeSeconds();
+	    System.out.println("dir does not exist");
+	}
     }
 
     public long getCurrentTimeSeconds() {
@@ -34,6 +43,36 @@ public class DTDate {
     public String daysElapsedString() {
         long days = calculateElapsedDays();
         return Long.toString(days);
+    }
+
+    // If the file is corrupt, return current time,
+    // and save it to a file.
+    //
+    // Otherwise, read the last time saved from a file
+    public long readInitTimeFromFile() {
+        BufferedReader reader = null;
+        long lInitTime = getCurrentTimeSeconds();
+
+	try {
+            reader = new BufferedReader(new FileReader(saveFileName));
+            String line = null;
+            if ((line = reader.readLine()) != null) {
+                lInitTime = Long.parseLong(line);
+            }
+	} catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                } 
+            }
+            catch (Exception e) {
+            }
+        }
+
+        saveStringToFile(saveFileName, Long.toString(lInitTime));
+        return lInitTime;
     }
 
     public void saveStringToFile(String file, String str) {
@@ -62,7 +101,7 @@ public class DTDate {
     public static void main(String[] args) {
         DTDate d = new DTDate();
         System.out.println(d.calculateElapsedDays());
-	d.saveCurrentTime();
+        d.saveCurrentTime();
         // d.saveStringToFile("test", d.daysElapsedString());
     }
 }
